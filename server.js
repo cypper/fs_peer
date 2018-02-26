30081e48fd633fdbfe84e090c0fca95fd6ff3491
Nazar Pokora <cypper1999@gmail.com>
2018-02-26 14:56:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var thisPeer = {
-	port: 1101,
-	ip: 'localhost'
+	port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	ip: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 }
 var server = require("./core").server;
 
